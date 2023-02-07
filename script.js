808237dfbef454c730f788ae01d73a671a964aef
gerhardadler <gerhardskartveit@gmail.com>
2023-02-07 10:09:44 +0100

    connected to api

--- script.js
+++ script.js
@@ -1,23 +1,96 @@
-/* HENTER INFORMASJON */
-var xmlhttp = new XMLHttpRequest();
-var url = "https://www.hvakosterstrommen.no/api/v1/prices/2023/01-31_NO5.json";
-
-xmlhttp.onreadystatechange =
-function() {
-  if (this.readyState == 4 && this.status == 200) {
-    var myArr = JSON.parse(this.responseText);
-    myFunction(myArr);
-  }
-};
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
-
-function myFunction(arr) {
-  var out = "";
-  var i;
-  for(i = 0; i < arr.length; i++) {
-    out += arr[i].NOK_per_kWh.toString();
-    out += "<br>"
-  }
-  document.getElementById("id01").innerHTML = out;
+const mode_color = {
+  ON: "#50e7f2",
+  OFF: "#f25050",
+  ECONOMY: "#53f250"
 }
+
+async function getRequest(url) {
+  const response = await fetch(url, {
+    method: "GET",
+    mode: "cors"
+  })
+  return response
+}
+
+async function postRequest(url, body) {
+  const response = await fetch(url, {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(body)
+  })
+  return response
+}
+
+async function getMode() {
+  const response = await getRequest("https://smartswitch.gerhardadler.no/get_mode")
+  const json = await response.json()
+  return json.mode
+}
+
+async function setMode(mode) {
+  const response = await postRequest("https://smartswitch.gerhardadler.no/set_mode", {
+    mode: mode
+  })
+  return response
+}
+
+async function getArea() {
+  const response = await getRequest("https://smartswitch.gerhardadler.no/get_area")
+  const json = await response.json()
+  return json.area
+}
+
+async function setArea(area) {
+  const response = await postRequest("https://smartswitch.gerhardadler.no/set_area", {
+    area: area
+  })
+  return response
+}
+
+function modeBtnClick(setmode) {
+  setMode(setmode)
+    .then((response) => {
+      if (response.status === 201) {
+        for (const mode in mode_color) {
+          if (mode === setmode) {
+            document.getElementById(mode.toLowerCase() + "_btn").style.backgroundColor = mode_color[mode]
+          } else {
+            document.getElementById(mode.toLowerCase() + "_btn").style.backgroundColor = "white"
+          }
+        }
+      } else {
+        alert("error")
+      }
+    })
+}
+
+document.getElementById("on_btn").addEventListener("click", () => modeBtnClick("ON"))
+document.getElementById("off_btn").addEventListener("click", () => modeBtnClick("OFF"))
+document.getElementById("economy_btn").addEventListener("click", () => modeBtnClick("ECONOMY"))
+
+document.getElementById("region-select").addEventListener("change", (event) => {
+  event.target.disabled = true
+  setArea(event.target.value)
+    .then((response) => {
+      if (response.status === 201) {
+        event.target.disabled = false
+      } else {
+        alert("error")
+      }
+    })
+})
+
+getMode()
+  .then((mode) => {
+    document.getElementById(mode.toLowerCase() + "_btn").style.backgroundColor = mode_color[mode]
+  })
+
+getArea()
+  .then((area) => {
+    document.getElementById("region-select").disabled = false
+    document.getElementById("region-select").value = area
+  })
